054eae7c01e59cbb82cf3a8146b4bd2df249b9c0
Jbltech <jubriloje@gmail.com>
2026-01-01 12:43:05 +0100

    'kk'

--- server.js
+++ server.js
@@ -20,7 +20,11 @@ import bcrypt from "bcryptjs";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin:["https://jbl-streaming-website.vercel.app", "http://localhost:5000", "http://localhost:5173"],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  credentials: true
+}));
 app.use(express.json());
 
 mongoose
